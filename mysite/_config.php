273aa1a481cf90065f7fe24731ddb62b42d2924f
balfourcraig <balfourcraig@gmail.com>
2016-09-06 10:20:30 +1200

    Reverted _config.php to its old state

--- mysite/_config.php
+++ mysite/_config.php
@@ -1,17 +1,8 @@
 <?php
-
 global $project;
 $project = 'mysite';
-
-global $databaseConfig;
-$databaseConfig = array(
-	'type' => 'MySQLDatabase',
-	'server' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'innovQT',
-	'path' => ''
-);
-
+global $database;
+$database = '';
+require_once('conf/ConfigureFromEnv.php');
 // Set the site locale
 i18n::set_locale('en_US');
